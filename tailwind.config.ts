3efbae41a10928c8650221f9276e6497c23ca972
dsmarcelo <celoygm@gmail.com>
2024-05-25 03:39:06 -0300

    chore: cleaning code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,9 +31,6 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      // text: {
-      //   'p-medium': 'text-xl';
-      // }
     },
     fontFamily: {
       sans: ['var(--font-openSans)'],
