8fb1b1191827a5f665d8936f1741f15197321dfc
dsmarcelo <celoygm@gmail.com>
2024-05-29 16:32:03 -0300

    fix: fix top part background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -11,47 +11,47 @@ export default function Home() {
   return (
     <div className="min-h-screen mx-auto flex flex-col justify-center items-center">
       <div className="w-full relative flex justify-center">
-        <div className="w-full dark-gradient">
-          <div className="mx-auto flex flex-col justify-center items-center px-4 md:px-10 lg:px-20 max-w-7xl">
-            <header className="flex items-center justify-center h-20">
-              <Image
-                className=""
-                src="/Logo.svg"
-                alt="Logo"
-                width={170}
-                height={0}
-                priority
-              />
-            </header>
-            <main className="w-full">
-              <section className="w-full my-[120px] flex flex-col gap-6 items-center justify-center font-normal text-center">
-                <motion.h1
-                  initial={{ y: 20, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  transition={{ duration: 0.3 }}>
-                  Sofreu um golpe e <span className="text-tertiary">perdeu</span> mais de <span className="text-secondary">R$4.000,00?</span>
-                </motion.h1>
-                <motion.div
-                  initial={{ opacity: 0, scale: 0.8 }}
-                  whileInView={{ opacity: 1, scale: 1 }}
-                  viewport={{ once: true }}
-                  className="gap-2 w-full justify-center">
-                  <p className="p-normal">
-                    Recupere seu dinheiro agora!
-                  </p>
-                  <PillButton className='w-full mx-auto max-w-96 mt-2'>
-                    <div
-                      className="flex items-center gap-3">
-                      <IoLogoWhatsapp className="w-5 h-5" />
-                      <span className="text-xl leading-none label-xlarge text-black">Entre em contato</span>
-                    </div>
-                    <CarretIcon className='h-8' />
-                  </PillButton>
-                </motion.div>
-              </section>
-            </main>
-          </div >
-        </div>
+        <div className="mx-auto z-20 flex flex-col justify-center items-center px-4 md:px-10 lg:px-20 max-w-7xl">
+          <header className="flex items-center justify-center h-20">
+            <Image
+              className=""
+              src="/Logo.svg"
+              alt="Logo"
+              width={170}
+              height={0}
+              priority
+            />
+          </header>
+          <main className="w-full">
+            <section className="w-full my-[120px] flex flex-col gap-6 items-center justify-center font-normal text-center">
+              <motion.h1
+                initial={{ y: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.3 }}>
+                Sofreu um golpe e <span className="text-tertiary">perdeu</span> mais de <span className="text-secondary">R$4.000,00?</span>
+              </motion.h1>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}
+                className="gap-2 w-full justify-center">
+                <p className="p-normal">
+                  Recupere seu dinheiro agora!
+                </p>
+                <PillButton className='w-full mx-auto max-w-96 mt-2'>
+                  <div
+                    className="flex items-center gap-3">
+                    <IoLogoWhatsapp className="w-5 h-5" />
+                    <span className="text-xl leading-none label-xlarge text-black">Entre em contato</span>
+                  </div>
+                  <CarretIcon className='h-8' />
+                </PillButton>
+              </motion.div>
+            </section>
+          </main>
+
+        </div >
+        <div className="w-full absolute top-0 left-0 h-full dark-gradient"></div>
       </div>
 
       <div className="px-4 md:px-10 lg:px-20 max-w-7xl">
