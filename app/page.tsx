b94d3ffc925b974ccb5ee855aea20a40d26e23cf
dsmarcelo <celoygm@gmail.com>
2024-05-29 17:39:34 -0300

    chore: info row flex-box

--- app/page.tsx
+++ app/page.tsx
@@ -55,12 +55,10 @@ export default function Home() {
       </div>
 
       <div className="px-4 md:px-10 lg:px-20 max-w-7xl">
-
-        <section className="w-full mb-20 flex flex-col items-center gap-12">
+        <section className="w-full mb-20 flex flex-col items-center gap-6 md:gap-12">
           <h2 className="">ENTENDA O GOLPE DO PIX:</h2>
-          <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 gap-12 md:grid-rows-1 ">
-
-            <div className="p-normal md:text-base">
+          <div className="flex flex-col sm:flex-row gap-6 md:gap-12">
+            <div className="p-small md:text-base sm:w-1/2">
               <p className="">
                 “Golpe do PIX” é um termo abrangente para se referir a diversas fraudes que têm acontecido envolvendo esse sistema de transações financeiras.
               </p>
@@ -74,7 +72,7 @@ export default function Home() {
                 Diversos outros tipos de golpe têm surgido e, havendo consumação do ato criminoso envolvendo o uso do PIX, o golpe é caracterizado.
               </p>
             </div>
-            <div className="relative">
+            <div className="relative aspect-square sm:w-1/2">
               <Image
                 className="rounded-3xl"
                 src="/Phone.jpg"
