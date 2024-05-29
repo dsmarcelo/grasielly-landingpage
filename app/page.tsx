8a297c8596d44b4c1cc04ab9f9fa5f9022c6fa49
dsmarcelo <celoygm@gmail.com>
2024-05-29 06:20:26 -0300

    feat: add info section

--- app/page.tsx
+++ app/page.tsx
@@ -9,50 +9,88 @@ import { Footer } from "./components/common/footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex-col items-center">
-      <div className="flex flex-col justify-center items-center px-4 lg:px-20 dark-gradient w-screen">
-        <header className="flex items-center justify-center h-20">
-          <Image
-            className=""
-            src="/Logo.svg"
-            alt="Logo"
-            width={170}
-            height={0}
-            priority
-          />
-        </header>
-        <main className="w-full max-w-7xl">
-          <section className="w-full my-[120px] flex flex-col gap-6 items-center justify-center font-normal text-center">
-            <motion.h1
-              initial={{ y: 20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.3 }}>
-              Sofreu um golpe e <span className="text-tertiary">perdeu</span> mais de <span className="text-secondary">R$4.000,00?</span>
-            </motion.h1>
-            <motion.div
-              initial={{ opacity: 0, scale: 0.8 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              viewport={{ once: true }}
-              className="gap-2 w-full justify-center">
-              <p className="p-normal">
-                Recupere seu dinheiro agora!
-              </p>
-              <PillButton className='w-full mx-auto max-w-96 mt-2'>
-                <div
-                  className="flex items-center gap-3">
-                  <IoLogoWhatsapp className="w-5 h-5" />
-                  <span className="text-xl leading-none label-xlarge text-black">Entre em contato</span>
-                </div>
-                <CarretIcon className='h-8' />
-              </PillButton>
-            </motion.div>
-          </section>
-        </main>
-      </div >
+    <div className="min-h-screen mx-auto flex flex-col justify-center items-center">
+      <div className="w-full relative flex justify-center">
+        <div className="w-full dark-gradient">
+          <div className="mx-auto flex flex-col justify-center items-center px-4 md:px-10 lg:px-20 max-w-7xl">
+            <header className="flex items-center justify-center h-20">
+              <Image
+                className=""
+                src="/Logo.svg"
+                alt="Logo"
+                width={170}
+                height={0}
+                priority
+              />
+            </header>
+            <main className="w-full">
+              <section className="w-full my-[120px] flex flex-col gap-6 items-center justify-center font-normal text-center">
+                <motion.h1
+                  initial={{ y: 20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.3 }}>
+                  Sofreu um golpe e <span className="text-tertiary">perdeu</span> mais de <span className="text-secondary">R$4.000,00?</span>
+                </motion.h1>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  whileInView={{ opacity: 1, scale: 1 }}
+                  viewport={{ once: true }}
+                  className="gap-2 w-full justify-center">
+                  <p className="p-normal">
+                    Recupere seu dinheiro agora!
+                  </p>
+                  <PillButton className='w-full mx-auto max-w-96 mt-2'>
+                    <div
+                      className="flex items-center gap-3">
+                      <IoLogoWhatsapp className="w-5 h-5" />
+                      <span className="text-xl leading-none label-xlarge text-black">Entre em contato</span>
+                    </div>
+                    <CarretIcon className='h-8' />
+                  </PillButton>
+                </motion.div>
+              </section>
+            </main>
+          </div >
+        </div>
+      </div>
 
-      <section className="px-4 lg:px-20">
-        <DropdownContainer />
-      </section>
+      <div className="px-4 md:px-10 lg:px-20 max-w-7xl">
+
+        <section className="w-full mb-20 flex flex-col items-center gap-12">
+          <h2 className="">ENTENDA O GOLPE DO PIX:</h2>
+          <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 gap-12 md:grid-rows-1 ">
+
+            <div className="p-normal md:text-base">
+              <p className="">
+                “Golpe do PIX” é um termo abrangente para se referir a diversas fraudes que têm acontecido envolvendo esse sistema de transações financeiras.
+              </p>
+              <p className="mt-4">
+                A natureza dos golpes pode variar, no entanto, grande parte deles envolve a obtenção indevida de dados do consumidor, possibilitando que suas contas bancárias sejam invadidas e que transações não autorizadas sejam feitas.
+              </p>
+              <p className="mt-4">
+                Também ocorrem simulações de boletos falsos, pelos quais o consumidor acredita estar pagando algo, mas na verdade acaba enviando dinheiro para uma conta diferente daquela que ele estava imaginando.
+              </p>
+              <p className="mt-4">
+                Diversos outros tipos de golpe têm surgido e, havendo consumação do ato criminoso envolvendo o uso do PIX, o golpe é caracterizado.
+              </p>
+            </div>
+            <div className="relative">
+              <Image
+                className="rounded-3xl"
+                src="/Phone.jpg"
+                alt="Pessoa segurando celular"
+                layout="fill"
+                objectFit="cover"
+                priority
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full ">
+          <DropdownContainer />
+        </section>
+      </div>
       <Footer />
     </div >
   );
